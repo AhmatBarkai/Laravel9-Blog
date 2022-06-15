f7b7976d991e0361732b81aa3ff2dcc8f33d8300
AhmatBarkai <100247496+AhmatBarkai@users.noreply.github.com>
2022-06-15 15:31:44 +0300

    Add files via upload

---
+++ database/migrations/2021_12_21_152232_create_f_a_q_s_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFAQSTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('f_a_q_s', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('question');
+            $table->text('answer');
+            $table->integer('position')->default(0);
+            $table->string('status', 5)->nullable()->default('False');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('f_a_q_s');
+    }
+}
